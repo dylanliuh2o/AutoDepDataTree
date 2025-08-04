b4ccf39a6903101e481cd14a7a5ce0d94fcac82c
dylanliuh2o <dylanliuh2o@foxmail.com>
2025-08-05 01:22:23 +0800

    test: add a simple test of process tree
    Add a simple test of process tree (generated by GitHub Copilot).

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,7 @@
-#include <iostream>
-
-#include <tabulate/table.hpp>
+#include "ProcessTreeDemo.h"
 
 int main()
 {
-    tabulate::Table table;
-    table.add_row({"PID", "Level", "CPU %", "Memory", "CPU % (local)", "Memory (local)"});
-    table.add_row({
-        "0",
-        "0",
-        "100.0",
-        "2000",
-        "100.0",
-        "2000"
-    });
-    std::cout << table << std::endl;
-
+    run_process_tree_demo();
     return 0;
 }
